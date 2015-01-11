39f1531305309108c66dc2ce54800b4d9b4ff869
kai nguyen <hungnh1704@gmail.com>
2015-01-11 17:28:35 +0700

    init
    removed unused files.

--- app/routes/calculator.routes.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kaizzige on 1/9/15.
- */
-
-var calculator = require('../controller/add.controller.js');
-
-module.exports = function (app) {
-    app.post('/add', calculator.add);
-}
